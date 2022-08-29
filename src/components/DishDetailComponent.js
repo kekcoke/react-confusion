70649b65bdd8b443aefdd78c07bdd68ecea7fb47
kekcoke <kevin.oane@yahoo.com>
2022-08-28 23:09:18 -0700

    Add DishDetail Component

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    formatCommentDate = (timestamp) => new Date(timestamp).toLocaleDateString('en-CA', { year: 'numeric', month: 'short', day: 'numeric'}); 
+
+    render() {
+        if (!!this.props.dish) {
+            return (
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        { this.renderComments(this.props.dish.comments) }
+                    </div>
+                </div>
+            );
+        } else {
+            return(<div></div>);
+        }
+    }
+
+    renderComments(comments) {
+        if (!comments || comments.length === 0) {
+            return(<div></div>);
+        }
+
+        const renderComments  = comments.map( (comment) => {
+            return (
+                <li>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author}, {this.formatCommentDate(comment.date)}</p>
+                </li>
+            )
+        });
+
+        return(
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    { renderComments }
+                </ul>
+            </div>
+        );
+    }
+
+    renderDish(dish) {
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    }
+}
+
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CarImgOverlay, CardText, CardBody, CardTitle, CardImgOverlay } from 'reactstrap';
+import { Card, CardImg, CardTitle, CardImgOverlay } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -17,15 +18,14 @@ class Menu extends Component {
     render() {
       const menu = this.props.dishes.map((dish) => {
           return (
-            <div  className="col-12 col-md-5 m-1">
-              <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-              </Card>
-            </div>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card onClick={() => this.onDishSelect(dish)}>
+              <CardImg width="100%" src={dish.image} alt={dish.name}/>
+              <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </div>
           );
       });
 
@@ -34,30 +34,9 @@ class Menu extends Component {
               <div className="row">
                   {menu}
               </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                  {this.renderDish(this.state.selectedDish)}
-                </div>
-              </div>
+              <DishDetail dish={this.state.selectedDish} />
           </div>
       );
-    }
-
-    renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
     }
 }
 
