843759217cd9fb12529fa506d693ccc3c46783ad
kekcoke <kevin.oane@yahoo.com>
2022-09-04 19:04:29 -0700

    Add ContactComponent + js objects to render on Home component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,11 @@ import Menu from './MenuComponent';
 import Footer from './Footer';
 import { Switch, Route, Redirect } from "react-router-dom";
 import DishDetail from "./DishDetailComponent";
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
 
@@ -13,6 +17,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
             selectedDish: null
         };
     }
@@ -24,7 +31,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return(
-                <Home />
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         return(
@@ -33,6 +44,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
